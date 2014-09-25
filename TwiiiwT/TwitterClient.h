b378b3a17d5e48e33e1ae4bb6a8a7614de04807e
peiqi <peiqi@yahoo-inc.com>
2014-09-24 22:41:05 -0700

    Add Podfile

---
+++ TwiiiwT/TwitterClient.h
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.h
+//  TwiiiwT
+//
+//  Created by Peiqi Zheng on 9/24/14.
+//  Copyright (c) 2014 Peiqi Zheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TwitterClient : NSObject
+
+@end
---
+++ TwiiiwT/TwitterClient.m
@@ -0,0 +1,17 @@
+//
+//  TwitterClient.m
+//  TwiiiwT
+//
+//  Created by Peiqi Zheng on 9/24/14.
+//  Copyright (c) 2014 Peiqi Zheng. All rights reserved.
+//
+
+#import "TwitterClient.h"
+
+NSString const *twitterApiBaseURL = @"https://api.twitter.com/";
+NSString const *twitterConsumerKey = @"WpcNCwLM1viqKjd7NAcc99Sea";
+NSString const *twitterConsumerSecret = @"3wyNpHA2nasmOeAvFQQy4NKZvUrSBrB1S4alAnVtehyK0Midhk";
+
+@implementation TwitterClient
+
+@end
